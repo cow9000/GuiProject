c3914ae9f45570f16f1120cae4f8155d9de66f5a
Derek Vawdrey <flyingpiechicken@gmail.com>
2017-11-13 12:58:27 -0700

    Created the setupLayout() method

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -8,20 +8,23 @@ import gui.controller.GuiController;
 public class GUIFrame extends JFrame
 {
 	private GuiController appController;
+	private GUIPanel appPanel;
 	
 	public GUIFrame(GuiController appController) {
 		super();
 		this.appController = appController;
-	
+		this.appPanel = new GUIPanel(appController);
+		
 		setupFrame();
 	
 	}
 	
 	private void setupFrame() {
 
+		this.setContentPane(appPanel);
+		
 		this.setTitle("");
 		this.setSize(500, 500);
-		
 		
 		this.setVisible(true);
 	}
--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -1,16 +1,57 @@
 package gui.view;
 
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
 import gui.controller.GuiController;
 
 public class GUIPanel extends JPanel
 {
 	private GuiController appController;
+	private JButton firstButton;
+	private SpringLayout appLayout;
+	
 	
 	public GUIPanel(GuiController appController) {
 		super();
 		this.appController = appController;
+		this.firstButton = new JButton("Lololol");
+		this.appLayout = new SpringLayout();
+
+		
+		setupPanel();
+		setupLayout();
+		
 	}
+	/**
+	 * Used to add all components to the screen and install a layout manager.
+	 * Also set color :D
+	 */
+	private void setupPanel() {
+		this.setBackground(new Color(10,20,200));
+		this.setLayout(appLayout);
+		this.add(firstButton);
+	
+	}
+	
+	/**
+	 * Used to hold the constraint positioning for springLayout.
+	 * AKA a giant dumping ground for all the xxx.putConstraint() lines
+	 */
+	
+	private void setupLayout() {
+		appLayout.putConstraint(SpringLayout.NORTH, firstButton, 33, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.WEST, firstButton, 171, SpringLayout.WEST, this);
+		
+		
+	}
+	
+	private void setupListeners() {
+		
+	}
+	
 	
 }
